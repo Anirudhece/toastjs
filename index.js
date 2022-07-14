a5a6f099a8110dff483ee2917151d793715e0fc4
anirudh <anirudhece2020@gmail.com>
2022-07-14 14:28:35 +0530

    final2.1

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let btn=document.querySelector(".btn");
 let pop=document.querySelector(".pop");
 btn.addEventListener("click",work);
-function work(){
+function work(e){
+    e.stopPropagation();
     pop.style.display="block";
-    setTimeout((e)=>{
+    setTimeout(()=>{
         pop.style.display="none";
-        e.stopPropagation();
     },3000);
 } 
